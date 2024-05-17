d20387fdfdbc70c88dc1a20f44ede4b0cd940ca3
Mohammad Hossein Khadem al - Mahdi <101788797+khadem-mh@users.noreply.github.com>
2024-05-17 18:40:09 +0330

    init project and coding logic

---
+++ app.js
@@ -0,0 +1,31 @@
+
+let timeToStart = document.querySelector('#timeToStart')
+let timeControl = document.querySelector('#timeControl')
+let second = 1000
+let minute = second * 60
+let hour = minute * 60
+let day = hour * 24
+
+// اون تاریخی که قراره بهش برسیم
+let countDown = new Date('Sep 20, 2025 00:00:00').getTime();
+
+const myRacing = () => {
+
+    let nowDate = new Date().getTime(),
+        distance = countDown - nowDate;
+    //
+    document.getElementById('days').innerText = Math.floor(distance / (day)),
+        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+
+    // وقتی تاریخ و زمان گذشته بود
+    if (distance < 0) {
+        clearInterval(MyTimer);
+        timeToStart.innerHTML = 'The camp began ☻'
+        timeControl.innerHTML = ''
+    }
+
+}
+
+MyTimer = setInterval(myRacing, 1000);
